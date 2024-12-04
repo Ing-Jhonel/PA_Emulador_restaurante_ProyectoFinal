a794d24c2e364e206adb6028b826890e4e625e92
Jhonel Apaza <jgapazap@unjbg.edu.pe>
2024-12-04 09:53:04 -0500

    Menu principal base

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+//#include "menus.h"
+using namespace std;
+
+int main(){
+	
+	int opcion;
+	do{
+		cout << "Menu Principal" << endl << endl
+			<< "1) Cliente" << endl
+			<< "2) Carta" << endl
+			<< "3) Finanza" << endl 
+			<< "4) Salir "<< endl << endl
+			<< "= "; cin >> opcion;
+			
+			switch(opcion){
+				case 1:
+					//menuCliente();
+					break;
+				case 2:
+					//menuCarta();
+					break;
+				case 3:
+					//menuFinanza();
+					break;
+				case 4:
+					cout << "Saliendo del programa." << endl;
+				default:
+					cout << "Indique una opcion valida." << endl;
+					break;
+			}
+	} while (opcion != 4);
+	return 0;
+}
